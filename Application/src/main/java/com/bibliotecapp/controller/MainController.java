a5b029ed527216bdfe689928ad3cce77cc115e2f
Thomas <sinan.thomas@gmail.com>
2019-10-28 16:41:53 +0100

    Hello world works, spring solution fully functionnal

--- Application/src/main/java/com/bibliotecapp/controller/MainController.java
+++ Application/src/main/java/com/bibliotecapp/controller/MainController.java
@@ -1,3 +1,5 @@
+package com.bibliotecapp.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
